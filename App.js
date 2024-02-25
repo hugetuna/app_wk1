68afa2c42f2c61818e66740fecc68a988894627a
hugetuna <113012817+hugetuna@users.noreply.github.com>
2024-02-25 18:39:56 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello world again for about 4 times/
+        111119007郭家豪
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  word: {
+    color: '#aaa'
+  },
+});
